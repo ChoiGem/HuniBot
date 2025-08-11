a06a73afc730a2f5406d58e13393adfb2160983e
ChoiGem <pk726510@gmail.com>
2025-08-11 22:38:36 +0900

    fix: 멘션방법 전역유저 찾기로 변경

--- src/main/java/org/example/bot/commands/DdubiCommand.java
+++ src/main/java/org/example/bot/commands/DdubiCommand.java
@@ -1,16 +1,15 @@
 package org.example.bot.commands;
 
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class DdubiCommand implements BotCommand {
+    String jjahwangUserId = "362114583592304640";
+
     @Override
     public void execute(MessageReceivedEvent event, String[] args) {
-        String jjahwangUserId = "362114583592304640";
-        User targetUser = event.getJDA().getUserById(jjahwangUserId);
-
-        if (targetUser != null) {
-            event.getChannel().sendMessage(targetUser.getAsMention() + " 견장없는석윤").queue();
-        }
+        event.getJDA().retrieveUserById(jjahwangUserId).queue(user -> {
+            event.getChannel().sendMessage(user.getAsMention() + " 견장없는석윤").queue();
+        }, failure -> {});
     }
 }
